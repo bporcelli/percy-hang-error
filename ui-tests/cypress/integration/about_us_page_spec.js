53c2459a59ea6c6327407c3e7c047dbefe501002
Brett Porcelli <brettporcelli@gmail.com>
2022-02-28 16:31:05 -0500

    fix: remove extraneous tests

--- ui-tests/cypress/integration/about_us_page_spec.js
+++
@@ -1,9 +0,0 @@
-// type definitions for Cypress object "cy"
-/// <reference types="cypress" />
-
-describe( 'Testing about us', function () {
-	it( 'Visits the about us page', function () {
-		cy.visit( '/about-us/' );
-		cy.percySnapshot();
-	} );
-} );
--- ui-tests/cypress/integration/blog_page_spec.js
+++
@@ -1,9 +0,0 @@
-// type definitions for Cypress object "cy"
-/// <reference types="cypress" />
-
-describe('Test Blog page', () => {
-	it('Visits the Blog page', () => {
-		cy.visit('/blog/');
-		cy.percySnapshot();
-	});
-});
